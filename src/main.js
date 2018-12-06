86c36ae4c6658187883cbaa66d11fff3f7fac254
zhengel <zhengenlong@chinasofti.com>
2018-12-06 23:56:33 +0800

    boot

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
-import 'bootstrap'
 import 'jquery'
 Vue.config.productionTip = false
 
